797fede7353f2ad6820528e371adb68fe96d289c
sleekcharly <c_ukasoanya@yahoo.com>
2020-11-25 00:57:31 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,149 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  KeyboardAvoidingView,
+  ImageBackground,
+  View,
+  ActivityIndicator,
+  StatusBar,
+} from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import SearchInput from "./components/SearchInput";
+
+import { fetchLocationId, fetchWeather } from "./utils/api";
+import getImageForWeather from "./utils/getImageForWeather";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      location: "",
+      loading: false,
+      error: false,
+      temperature: 0,
+      weather: "",
+    };
+  }
+
+  componentDidMount() {
+    this.handleUpdateLocation("Lagos");
+  }
+
+  handleUpdateLocation = async (city) => {
+    if (!city) return;
+
+    this.setState({ loading: true }, async () => {
+      try {
+        const locationId = await fetchLocationId(city);
+        const { location, weather, temperature } = await fetchWeather(
+          locationId
+        );
+
+        this.setState({
+          loading: false,
+          error: false,
+          location,
+          weather,
+          temperature,
+        });
+      } catch (e) {
+        this.setState({
+          loading: false,
+          error: true,
+        });
+      }
+    });
+  };
+
+  render() {
+    const { location, error, loading, weather, temperature } = this.state;
+
+    return (
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <StatusBar barStyle="light-content" />
+        <ImageBackground
+          source={getImageForWeather(weather)}
+          style={styles.imageContainer}
+          imageStyle={styles.image}
+        >
+          <View style={styles.detailsContainer}>
+            <ActivityIndicator animating={loading} color="white" size="large" />
+
+            {!loading && (
+              <View>
+                {error && (
+                  <Text style={[styles.smallText, styles.textStyle]}>
+                    Could not load weather, please try a different city
+                  </Text>
+                )}
+
+                {!error && (
+                  <View>
+                    <Text style={[styles.largeText, styles.textStyle]}>
+                      {location}
+                    </Text>
+                    <Text style={[styles.smallText, styles.textStyle]}>
+                      {weather}
+                    </Text>
+                    <Text
+                      style={[styles.largeText, styles.textStyle]}
+                    >{`${Math.round(temperature)}°`}</Text>
+                  </View>
+                )}
+
+                <SearchInput
+                  placeholder="Search any city"
+                  onSubmit={this.handleUpdateLocation}
+                />
+              </View>
+            )}
+          </View>
+        </ImageBackground>
+      </KeyboardAvoidingView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#34495E",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  textStyle: {
+    textAlign: "center",
+    ...Platform.select({
+      ios: {
+        fontFamily: "AvenirNext-Regular",
+      },
+      android: {
+        fontFamily: "Roboto",
+      },
+    }),
+    color: "white",
+  },
+  largeText: {
+    fontSize: 44,
+  },
+  smallText: {
+    fontSize: 18,
+  },
+
+  image: {
+    flex: 1,
+    width: null,
+    height: null,
+    resizeMode: "cover",
+  },
+  detailsContainer: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "rgba(0,0,0,0.2)",
+    paddingHorizontal: 20,
   },
 });
